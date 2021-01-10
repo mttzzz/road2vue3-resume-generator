b421874d42f653706c21ed846dd605523864febf
Kirill Egorov <kirill_egorov@me.com>
2021-01-10 23:03:04 +0300

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# если вы публикуете по адресу https://<USERNAME>.github.io
+# 1если вы публикуете по адресу https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
